113ce0ed1d8e074ec23dedbfe8a09044afb4f60a
rohitbora178 <rohitbora178@gmail.com>
2024-10-09 21:49:39 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 const App = () => {
-  const [count , setCount] = useState(0);
-
-  const handleClick = () => {
-    setCount(count + 1);
-  }
-
-  useEffect(()=>{
-    // Add a timer to increment the count every second
-    const timer = setInterval(() => {
-      setCount(count + 1);
-    }, 1000);
-
-    // Clean up the timer when the component is unmounted
-    return () => clearInterval(timer);
-  })
-
-
   return (
-   <>
-    <h1>Count: {count}</h1>
-    <button onClick={handleClick}>Increment</button>
-   </>
+    <div>App</div>
   )
 }
 
-export default App;
+export default App
