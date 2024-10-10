9b8e4b24811310632b9b7a080acfe278992c620d
rohitbora178 <rohitbora178@gmail.com>
2024-10-10 10:13:36 +0530

    fetchDataEmpty

--- src/App.js
+++ src/App.js
@@ -1,36 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 const App = () => {
-  const [data, setData] = useState(0);
-
-  const fetchData = async () => {
-    try {
-      const response = await fetch('https://api.example.com/data');
-      const json = await response.json();
-      setData(json.data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  }
-
-  useEffect(()=>{
-    fetchData();
-  } , [])
   return (
-   <>
-   {data.map((data)=>(
-<div >
-   <h1>{data.title}</h1>
-   <p>{data.description}</p>
-   <img src={data.image} alt={data.title} />
-   <p>Author: {data.author}</p>
-   <p>Date: {data.date}</p>
-   <button onClick={()=> fetchData()}>Fetch Data</button>
-   <hr />
-   
-</div>
-   ))}
-   </>
+    <div>App</div>
   )
 }
 
